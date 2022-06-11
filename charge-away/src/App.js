e48b50d295397509ed389378293b4ab582333c01
Jomari Camangon <jomari60@hotmail.co.uk>
2022-06-11 22:02:47 +0100

    added Footer and Navbar components, components dub folder added.

--- charge-away/src/App.js
+++ charge-away/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
-import LanddingPage from './components/LandingPage.js';
-import BigMap from "./components/BigMap.js";
+import LanddingPage from './components/LandingPage/LandingPage.js';
+import BigMap from "./components/BigMap/BigMap.js";
+import NavBar from "./components/NavBar/NavBar.js";
+import Footer from "./components/Footer/Footer.js";
 
 function App() {
   return (
-    <> 
+    <>
+    <NavBar />
     <LanddingPage /> 
     <BigMap /> 
+    <Footer />
     </>
   );
 }
